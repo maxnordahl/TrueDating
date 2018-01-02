93e88a26cbd6280b99d66cb03502911cd3d4739f
Esho Odisho <esho96@hotmail.com>
2018-01-02 13:00:26 +0100

    Sökfunktion

--- TrueDating/Controllers/UsersController.cs
+++ TrueDating/Controllers/UsersController.cs
@@ -4,15 +4,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace TrueDating.Controllers
 {
     public class UsersController : BaseController
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index(String searchString)
         {
-            var users = db.Users.ToList();
-            return View(users);
+            //var users = db.Users.ToList();
+            var users = from u in db.Users
+                        select u;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                users = users.Where(s => s.Nickname.Contains(searchString));
+            }
+
+            return View(await users.ToListAsync());
+            //return View(users);
         }
+
+       
     }
 }
